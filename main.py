428737cd150b52c731803f0030c6a95c4373177d
Aim <7asim6@gmail.com>
2025-04-14 20:26:57 +0400

    County,Weather,Temp

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import requests as re
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import messagebox
+
+API_KEY = "9acd0597ae349fa651ff17893ec5b7e2"
+
+
 
 root = tk.Tk()
 root.title('Weather App')
@@ -12,7 +17,11 @@ city_entry = tk.Entry(root, font=("Arial", 18))
 city_entry.place(x=20,y=110)
 
 def prt():
-    print(city_entry.get())
+    city = city_entry.get()
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    response = re.get(url)
+    data = response.json()
+    messagebox.showinfo(f"Weather",f"Country : {data["sys"]["country"]}, Weather : {data["weather"][0]["main"]}, Temp : {int(data["main"]["temp"])}")
 
 enter_button = tk.Button(root,text="Enter",font=("Arial", 15, "bold"),command=prt)
 enter_button.place(x=125,y=170)
