0ff9290b69dd71245d28bf1a82f0edff15ff9e53
Aim <7asim6@gmail.com>
2025-04-14 19:57:27 +0400

    ""

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ root.geometry("300x300")
 title = tk.Label(root,text="Weather App",font=("Arial", 20, "bold"))
 title.place(x=65,y=50)
 
-enter_button = tk.Button(root,text="Enter",font=("Arial", 15, "bold"))
-enter_button.place(x=125,y=170)
-
 city_entry = tk.Entry(root, font=("Arial", 18))
 city_entry.place(x=20,y=110)
+
+def prt():
+    print(city_entry.get())
+
+enter_button = tk.Button(root,text="Enter",font=("Arial", 15, "bold"),command=prt)
+enter_button.place(x=125,y=170)
 
 root.mainloop()
